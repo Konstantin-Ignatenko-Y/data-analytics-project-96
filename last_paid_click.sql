630a84234c688a2162b7c2fee81d6e13cdaaec26
Konstantin-Ignatenko-Y <109867015+Konstantin-Ignatenko-Y@users.noreply.github.com>
2026-05-20 13:53:53 +0300

    Add files via upload

--- last_paid_click.sql
+++ last_paid_click.sql
@@ -1,38 +1,56 @@
+WITH paid_visits AS (
+    SELECT
+        s.visitor_id,
+        s.visit_date,
+        COALESCE(ya.utm_source, vk.utm_source) AS utm_source,
+        COALESCE(ya.utm_medium, vk.utm_medium) AS utm_medium,
+        COALESCE(ya.utm_campaign, vk.utm_campaign) AS utm_campaign,
+        ROW_NUMBER() OVER (
+            PARTITION BY s.visitor_id
+            ORDER BY s.visit_date DESC
+        ) AS rn
+    FROM sessions AS s
+    LEFT JOIN ya_ads AS ya
+        ON
+            s.source = ya.utm_source
+            AND s.medium = ya.utm_medium
+            AND s.campaign = ya.utm_campaign
+            AND s.content = ya.utm_content
+    LEFT JOIN vk_ads AS vk
+        ON
+            s.source = vk.utm_source
+            AND s.medium = vk.utm_medium
+            AND s.campaign = vk.utm_campaign
+            AND s.content = vk.utm_content
+    WHERE
+        ya.utm_medium <> 'organic' OR vk.utm_medium <> 'organic'
+),
+
+last_paid_click AS (
+    SELECT *
+    FROM paid_visits
+    WHERE rn = 1
+)
+
 SELECT DISTINCT
-    s.visitor_id,
-    s.visit_date,
+    lpc.visitor_id,
+    lpc.visit_date,
+    lpc.utm_source,
+    lpc.utm_medium,
+    lpc.utm_campaign,
     l.lead_id,
     l.created_at,
     l.amount,
     l.closing_reason,
-    l.status_id,
-    COALESCE(ya.utm_source, vk.utm_source) AS utm_source,
-    COALESCE(ya.utm_medium, vk.utm_medium) AS utm_medium,
-    COALESCE(ya.utm_campaign, vk.utm_campaign) AS utm_campaign
-FROM sessions AS s
-LEFT JOIN leads AS l ON s.visitor_id = l.visitor_id
-LEFT JOIN ya_ads AS ya
+    l.status_id
+FROM last_paid_click AS lpc
+LEFT JOIN leads AS l
     ON
-        s.source = ya.utm_source
-        AND s.medium = ya.utm_medium
-        AND s.campaign = ya.utm_campaign
-        AND s.content = ya.utm_content
-LEFT JOIN vk_ads AS vk
-    ON
-        s.source = vk.utm_source
-        AND s.medium = vk.utm_medium
-        AND s.campaign = vk.utm_campaign
-        AND s.content = vk.utm_content
-WHERE
-    (
-        ya.utm_medium IN ('cpc', 'cpm', 'cpa', 'youtube', 'cpp', 'tg', 'social')
-        OR vk.utm_medium IN (
-            'cpc', 'cpm', 'cpa', 'youtube', 'cpp', 'tg', 'social'
-        )
-    )
+        lpc.visitor_id = l.visitor_id
+        AND (lpc.visit_date <= l.created_at OR l.created_at IS NULL)
 ORDER BY
     l.amount DESC NULLS LAST,
-    s.visit_date ASC,
-    COALESCE(ya.utm_source, vk.utm_source) ASC,
-    COALESCE(ya.utm_medium, vk.utm_medium) ASC,
-    COALESCE(ya.utm_campaign, vk.utm_campaign) ASC;
+    lpc.visit_date ASC,
+    lpc.utm_source ASC,
+    lpc.utm_medium ASC,
+    lpc.utm_campaign ASC;
