cf3e4edda4c6663636babe9dd6009c9e64b789b2
Konstantin-Ignatenko-Y <109867015+Konstantin-Ignatenko-Y@users.noreply.github.com>
2026-05-25 10:03:30 +0300

    Add files via upload

---
+++ last_paid_clic.sql
@@ -0,0 +1,44 @@
+WITH visitors_with_leads AS (
+    SELECT
+        s.visitor_id,
+        s.visit_date,
+        l.lead_id,
+        l.created_at,
+        l.amount,
+        l.closing_reason,
+        l.status_id,
+        s.medium AS utm_medium,
+        s.campaign AS utm_campaign,
+        LOWER(s.source) AS utm_source,
+        ROW_NUMBER() OVER (
+            PARTITION BY s.visitor_id
+            ORDER BY s.visit_date DESC
+        ) AS rn
+    FROM sessions AS s
+    LEFT JOIN leads AS l
+        ON
+            s.visitor_id = l.visitor_id
+            AND s.visit_date <= l.created_at
+    WHERE s.medium != 'organic'
+)
+
+SELECT
+    visitor_id,
+    visit_date,
+    utm_source,
+    utm_medium,
+    utm_campaign,
+    lead_id,
+    created_at,
+    amount,
+    closing_reason,
+    status_id
+FROM visitors_with_leads
+WHERE rn = 1
+ORDER BY
+    amount DESC NULLS LAST,
+    visit_date ASC,
+    utm_source ASC,
+    utm_medium ASC,
+    utm_campaign ASC
+LIMIT 10;
