6952e5ea77dd032f9dc31b9ac25fd64cb161f004
Konstantin-Ignatenko-Y <109867015+Konstantin-Ignatenko-Y@users.noreply.github.com>
2026-05-25 10:04:55 +0300

    Add files via upload

--- last_paid_click.sql
+++ last_paid_click.sql
@@ -1,57 +1,44 @@
-WITH paid_visits AS (
+WITH visitors_with_leads AS (
     SELECT
         s.visitor_id,
         s.visit_date,
-        COALESCE(ya.utm_source, vk.utm_source) AS utm_source,
-        COALESCE(ya.utm_medium, vk.utm_medium) AS utm_medium,
-        COALESCE(ya.utm_campaign, vk.utm_campaign) AS utm_campaign,
-		-- Реализуем счетчик для вычисления последнего платного клика
+        l.lead_id,
+        l.created_at,
+        l.amount,
+        l.closing_reason,
+        l.status_id,
+        s.medium AS utm_medium,
+        s.campaign AS utm_campaign,
+        LOWER(s.source) AS utm_source,
         ROW_NUMBER() OVER (
             PARTITION BY s.visitor_id
             ORDER BY s.visit_date DESC
         ) AS rn
     FROM sessions AS s
-    LEFT JOIN ya_ads AS ya
+    LEFT JOIN leads AS l
         ON
-            s.source = ya.utm_source
-            AND s.medium = ya.utm_medium
-            AND s.campaign = ya.utm_campaign
-            AND s.content = ya.utm_content
-    LEFT JOIN vk_ads AS vk
-        ON
-            s.source = vk.utm_source
-            AND s.medium = vk.utm_medium
-            AND s.campaign = vk.utm_campaign
-            AND s.content = vk.utm_content
-    WHERE
-        ya.utm_medium <> 'organic' OR vk.utm_medium <> 'organic'
-),
-
-last_paid_click AS (
-    SELECT *
-    FROM paid_visits
-    WHERE rn = 1
+            s.visitor_id = l.visitor_id
+            AND s.visit_date <= l.created_at
+    WHERE s.medium != 'organic'
 )
 
-SELECT DISTINCT
-    lpc.visitor_id,
-    lpc.visit_date,
-    lpc.utm_source,
-    lpc.utm_medium,
-    lpc.utm_campaign,
-    l.lead_id,
-    l.created_at,
-    l.amount,
-    l.closing_reason,
-    l.status_id
-FROM last_paid_click AS lpc
-LEFT JOIN leads AS l
-    ON
-        lpc.visitor_id = l.visitor_id
-        AND (lpc.visit_date <= l.created_at OR l.created_at IS NULL)
+SELECT
+    visitor_id,
+    visit_date,
+    utm_source,
+    utm_medium,
+    utm_campaign,
+    lead_id,
+    created_at,
+    amount,
+    closing_reason,
+    status_id
+FROM visitors_with_leads
+WHERE rn = 1
 ORDER BY
-    l.amount DESC NULLS LAST,
-    lpc.visit_date ASC,
-    lpc.utm_source ASC,
-    lpc.utm_medium ASC,
-    lpc.utm_campaign ASC;
+    amount DESC NULLS LAST,
+    visit_date ASC,
+    utm_source ASC,
+    utm_medium ASC,
+    utm_campaign ASC
+LIMIT 10;
